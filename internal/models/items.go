ae2d4d731dbce00c33707971f4f9292aac640a59
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-07-15 18:52:39 -0600

    Added functions for item model

--- internal/models/items.go
+++ internal/models/items.go
@@ -1,5 +1,7 @@
 package models
 
+import "errors"
+
 type ItemCategory int
 
 const (
@@ -19,9 +21,51 @@ type Effect struct {
 }
 
 type Item struct {
-	Name        string
-	Description string
-	Type        ItemCategory
-	Effect      Effect
-	Value       int32
+	name        string
+	description string
+	category    ItemCategory
+	effect      Effect
+	value       int32
+}
+
+func NewItem(name, description string, category ItemCategory, effect Effect, value int32) (*Item, error) {
+	if name == "" {
+		return nil, errors.New("invalid item name")
+	}
+	if description == "" {
+		return nil, errors.New("invalid item description")
+	}
+	if value < 0 {
+		return nil, errors.New("invalid item value")
+	}
+
+	item := Item{
+		name:        name,
+		description: description,
+		category:    category,
+		effect:      effect,
+		value:       value,
+	}
+
+	return &item, nil
+}
+
+func (i *Item) GetName() string {
+	return i.name
+}
+
+func (i *Item) GetDescription() string {
+	return i.description
+}
+
+func (i *Item) GetCategory() ItemCategory {
+	return i.category
+}
+
+func (i *Item) GetEffect() Effect {
+	return i.effect
+}
+
+func (i *Item) GetValue() int32 {
+	return i.value
 }
