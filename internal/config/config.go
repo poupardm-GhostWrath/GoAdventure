fa99e48511d872c3845b51e1fda29a1f5b1e66ad
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-07-16 10:00:08 -0600

    Set GlobalConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,19 +1,75 @@
 package config
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"log/slog"
+	"os"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/poupardm-GhostWrath/GoAdventure/internal/database"
 )
 
 var Cfg GlobalConfig
 
 type GlobalConfig struct {
-	ENV    string
-	DB     *pgx.Conn
-	Logger *slog.Logger
+	ENV       string
+	DB        *pgx.Conn
+	DBQueries *database.Queries
+	Logger    *slog.Logger
 }
 
 func init() {
+	// Get Environmental Variables
+	env := os.Getenv("ENV")
+	if env == "" {
+		log.Fatal("ENV not set")
+	}
 
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		log.Fatal("DB_NAME not set")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		log.Fatal("DB_USER not set")
+	}
+
+	dbPass := os.Getenv("DB_PASSWORD")
+	if dbPass == "" {
+		log.Fatal("DB_PASS not set")
+	}
+
+	dbAddr := os.Getenv("DB_ADDR")
+	if dbAddr == "" {
+		log.Fatal("DB_ADDR not set")
+	}
+
+	dbPort := os.Getenv("DB_PORT")
+	if dbPort == "" {
+		log.Fatal("DB_PORT not set")
+	}
+
+	dbURL := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		dbUser,
+		dbPass,
+		dbAddr,
+		dbPort,
+		dbName)
+
+	db, err := pgx.Connect(context.Background(), dbURL)
+	if err != nil {
+		log.Fatalf("Unable to connect to DB: %v\n", err)
+	}
+
+	dbQueries := database.New(db)
+
+	Cfg = GlobalConfig{
+		ENV:       env,
+		DB:        db,
+		DBQueries: dbQueries,
+	}
 }
