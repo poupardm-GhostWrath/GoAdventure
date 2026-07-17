b0e4b74e11ff24c51db35c1584c11be670ce4057
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-07-16 21:42:54 -0600

    Init and main.go basic setup (WIP)

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,17 @@ package config
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"log/slog"
 	"os"
+	"path"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/poupardm-GhostWrath/GoAdventure/internal/database"
+	"github.com/poupardm-GhostWrath/GoAdventure/internal/models"
 )
 
 var Cfg GlobalConfig
@@ -19,6 +23,8 @@ type GlobalConfig struct {
 	DBQueries *database.Queries
 	Logger    *slog.Logger
 }
+
+var ItemsList = make(map[string]*models.Item)
 
 func init() {
 	// Get Environmental Variables
@@ -52,6 +58,7 @@ func init() {
 		log.Fatal("DB_PORT not set")
 	}
 
+	// Connect to DB
 	dbURL := fmt.Sprintf(
 		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
 		dbUser,
@@ -71,5 +78,99 @@ func init() {
 		ENV:       env,
 		DB:        db,
 		DBQueries: dbQueries,
+	}
+
+	// Initialize Items
+	err = dbQueries.DeleteItems(context.Background())
+	log.Println("Clearing items from DB...")
+	if err != nil {
+		log.Fatalf("Failed to clear items in DB: %v\n", err)
+	}
+	log.Println("Items cleared from DB")
+
+	type ItemData struct {
+		Description  string `json:"description"`
+		EffectTarget string `json:"effect_target"`
+		EffectValue  int32  `json:"effect_value"`
+		Value        int32  `json:"value"`
+	}
+
+	log.Println("Loading Items...")
+	itemFiles := []string{"Medicine", "Meal"}
+	for _, itemFileName := range itemFiles {
+		dbItemCategory, err := dbQueries.GetItemCategoriesByName(context.Background(), itemFileName)
+		if err != nil {
+			log.Fatalf("Failed to get category id: %v\n", err)
+		}
+
+		fileName := fmt.Sprintf("%s.json", itemFileName)
+		filePath := path.Join("./internal/config", fileName)
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Fatalf("Failed to open file %s: %v\n", filePath, err)
+		}
+		var dataMap map[string]json.RawMessage
+		err = json.Unmarshal(data, &dataMap)
+		if err != nil {
+			log.Fatalf("Failed to unmarshal data: %v\n", err)
+		}
+
+		for itemName, rawItemData := range dataMap {
+			var itemData ItemData
+			err := json.Unmarshal(rawItemData, &itemData)
+			if err != nil {
+				log.Fatalf("Failed to unmarshal item data: %v\n", err)
+			}
+			dbItem, err := dbQueries.CreateItems(context.Background(), database.CreateItemsParams{
+				Name:        itemName,
+				Description: itemData.Description,
+				CategoryID:  dbItemCategory.ID,
+				EffectTarget: pgtype.Text{
+					String: itemData.EffectTarget,
+					Valid:  (itemData.EffectTarget != ""),
+				},
+				EffectValue: pgtype.Int4{
+					Int32: itemData.EffectValue,
+					Valid: (itemData.EffectValue != 0),
+				},
+				Value: itemData.Value,
+			})
+			if err != nil {
+				log.Fatalf("Failed to create item in DB: %v\n", err)
+			}
+			var itemCategory models.ItemCategory
+			switch fileName {
+			case "medicine":
+				itemCategory = models.Medicine
+			case "meal":
+				itemCategory = models.Meal
+			}
+			item, err := models.NewItem(
+				dbItem.Name,
+				dbItem.Description,
+				itemCategory,
+				models.Effect{
+					Target: dbItem.EffectTarget.String,
+					Value:  dbItem.EffectValue.Int32,
+				},
+				dbItem.Value,
+			)
+			if err != nil {
+				log.Fatalf("Failed to create item in memory: %v\n", err)
+			}
+
+			ItemsList[dbItem.Name] = item
+		}
+
+		log.Println("Loading Complete!")
+	}
+	for _, item := range ItemsList {
+		log.Println("------- Item -------")
+		log.Printf("Name: %s\n", item.GetName())
+		log.Printf("Description: %s\n", item.GetDescription())
+		log.Printf("Effect Target: %s\n", item.GetEffect().Target)
+		log.Printf("Effect Value: %d\n", item.GetEffect().Value)
+		log.Printf("Value: %d\n", item.GetValue())
+		log.Println()
 	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/poupardm-GhostWrath/GoAdventure/internal/config"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	status := run(ctx, cancel)
+	cancel()
+	defer func() {
+		if err := config.Cfg.DB.Close(ctx); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to close db connection: %v\n", err)
+		}
+	}()
+	os.Exit(status)
+}
+
+func run(ctx context.Context, cancel context.CancelFunc) int {
+	<-ctx.Done()
+	_, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return 0
 }
