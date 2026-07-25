69502ec7e71eda580923c292f90d5a1803fde363
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-07-25 12:20:07 -0600

    Moved health, mana and stat structs to stats.go

--- internal/models/players.go
+++ internal/models/players.go
@@ -26,21 +26,6 @@ type level struct {
 	currentLevel int32
 }
 
-type health struct {
-	currentHealth int32
-	maxHealth     int32
-}
-
-type mana struct {
-	currentMana int32
-	maxMana     int32
-}
-
-type stat struct {
-	strength int32
-	defense  int32
-}
-
 type Buff struct {
 	target   string
 	value    int32
---
+++ internal/models/stats.go
@@ -0,0 +1,16 @@
+package models
+
+type health struct {
+	currentHealth int32
+	maxHealth     int32
+}
+
+type mana struct {
+	currentMana int32
+	maxMana     int32
+}
+
+type stat struct {
+	strength int32
+	defense  int32
+}
